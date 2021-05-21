4dcf243807280f5685f1878db001e53c5eacedb3
cirol <cirol@192.168.2.59>
2021-05-21 12:55:57 -0300

    Dollar Converter

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.CurrencyConverter;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("What is the dollar price? ");
+		double dollar = sc.nextDouble();
+		System.out.println("How many dollar will be bought? ");
+		double quantity = sc.nextDouble();
+
+		double ressult = CurrencyConverter.calc(dollar, quantity);
+
+		System.out.printf("Amount to be paid in reais: R$ %.2f", ressult);
+
+		sc.close();
+
+	}
+
+}
---
+++ src/entities/CurrencyConverter.java
@@ -0,0 +1,10 @@
+package entities;
+
+public class CurrencyConverter {
+
+	public static double calc(double dollar, double quantity) {
+		return dollar * quantity * 1.06;
+	}
+	
+	
+}
